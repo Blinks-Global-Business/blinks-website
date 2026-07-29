2dfa55ea454e371d0ff29f2e7e6c809b39ce89ef
unknown <your_emailexample.com>
2026-07-29 17:26:34 +0200

    Ajout du nouvel employé

--- data/team.js
+++ data/team.js
@@ -15,12 +15,18 @@ export const TEAM_MEMBERS = [
     name: "Aïcha Rassoul Lo",
     role: { fr: "Assistante développement commerciale & communication digitale", en: "Business Development & Digital Communication Assistant" },
     photo: "/images/team/aichalo.jpeg",
-    linkedin: "https://www.linkedin.com/in/mick-aurel-zola-bb01a9293?utm_source=share_via&utm_content=profile&utm_medium=member_android",
+    linkedin: "https://www.linkedin.com/in/sokhna-aïssatou-lo-19018b426?utm_source=share_via&utm_content=profile&utm_medium=member_ios",
   },
   {
     name: "Mike-Aurel ZOLA",
     role: { fr: "Développeur web FullStack", en: "Full Stack Web Developer" },
     photo: "/images/team/mikezola.jpeg",
     linkedin: "https://www.linkedin.com/in/mick-aurel-zola-bb01a9293?utm_source=share_via&utm_content=profile&utm_medium=member_android",
+  },
+  {
+    name: "Casanova KOUAKANOU",
+    role: { fr: "Développeur Application mobile", en: "Mobile App Developer" },
+    photo: "/images/team/casanova.jpeg",
+    linkedin: "https://www.linkedin.com/in/ckoua4?utm_source=share_via&utm_content=profile&utm_medium=member_android",
   },
 ];
